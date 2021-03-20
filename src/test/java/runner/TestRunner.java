77ff13f3870e70db4132caa4820fa5f19439effd
umaganni <amsg.lon@gmail.com>
2021-03-20 17:20:17 +0000

    First Commit

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,47 @@
+/**
+ *
+ */
+package runner;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import utils.BrowserFactory;
+import utils.ConfigFileReader;
+import utils.Utilities;
+
+import java.util.Properties;
+
+
+@CucumberOptions(
+
+        features = {"src/test/java/features/"},
+        glue = {"stepDefinitions"},
+        monochrome = true,
+
+        plugin = {"pretty", "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/CucumberTestReport.html",
+                "json:target/cucumber-reports/CucumberTestReport.json"}
+
+
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    static Properties prop;
+
+
+
+    @BeforeClass
+    public static void setup() {
+        prop = ConfigFileReader.loadProperties();
+        BrowserFactory.getDriver();
+    }
+
+    @AfterClass
+    public static void afterClass(){
+        Utilities.loadReport();
+        BrowserFactory.close();
+    }
+
+}
